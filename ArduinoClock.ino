68e9d8ad9caf519575bff7d800ad4ee716e0cf45
Wouter <wouterhabets@gmail.com>
2013-12-21 19:48:21 +0100

    Variabelen toegevoegd

--- ArduinoClock.ino
+++ ArduinoClock.ino
@@ -1,12 +1,33 @@
 #include <LiquidCrystal.h>
 #include <Time.h>
 
+const int pinRX = 0;
+const int pinTX = 1;
+const int pinBacklight = 6;
+const int pinButtonBack = 7;
+const int pinButtonEnter = 8;
+const int pinButtonPrevious = 9;
+const int pinButtonNext = 10;
+const int pinBuzzer = 13;
+const int pinLightSensor = A0;
+const String dayOfWeek[] =
+	{"Maandag", "Dinsdag", "Woensdag", "Donderdag", "Vrijdag", "Zaterdag", "Zondag"};
+
+boolean buttonStateBack = false;
+boolean buttonStateEnter = false;
+boolean buttonStatePrevious = false;
+boolean buttonStateNext = false;
+
+int editHour, editMinute, editSecond, editDay, editMonth, editYear, editDOW;
+int lightVal = 255;
+int mode = 0;
+
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+
 void setup() {
-  // put your setup code here, to run once:
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
 
 }
